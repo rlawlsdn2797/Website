e6d721ab8446839c5c68980ec88ceee3b6f90c1f
김진우 <rlawlsdn2797@gmail.com>
2024-03-19 21:37:01 +0900

    1-2일차
    로그인 구현.
    try catch로 유저 수 구함.

--- src/Server.java
+++ src/Server.java
@@ -10,12 +10,24 @@ public class Server {
 	
 	private static void startsystem() {
 		//UserList[0] = new User();
-		//UserList[0].Register(2004, 12, 7, "KIM", "JINWOO", "ADMIN");
+		//UserList[0].register(2004, 12, 7, "KIM", "JINWOO", "ADMIN", "kjw2797", "jin5858");
 		//FileSave(UserList);
 		
 		FileLoad();
-		UserCount = UserList.length;
-		UserList[0].Print();
+		UserCount = Length();
+	}
+	
+	private static int Length() {
+		for(int i = 0; i < MaxUserCount; i++) {
+			try {
+				UserList[i].print();
+			}
+			catch (NullPointerException e) {
+				return i;
+			}
+		}
+		
+		return 100;
 	}
 	
 	private static void FileSave(User[] u) {
@@ -48,13 +60,32 @@ public class Server {
             return;
         }
 	}
+	
+	private static boolean Login(String id, String pw) {
+		for(int i = 0; i < UserCount; i++) {
+			if(UserList[i].idCompare(id)) {
+				if(UserList[i].pwCompare(pw)) {
+					System.out.println("로그인 성공");
+					return true;
+				}
+				else {
+					System.out.println("비밀번호가 틀렸습니다.");
+					return false;
+				}
+			}
+		}
+		
+		System.out.println("존재하지 않는 아이디입니다.");
+		return false;
+	}
 }
 
 class User implements Serializable  {	
-	int BirthY, BirthM, BirthD;
-	String LastName, RealName, NickName;
+	private int BirthY, BirthM, BirthD;
+	private String LastName, RealName, NickName;
+	private String id, pw;
 		
-	public void Register(int t_y, int t_m, int t_d, String ln, String rn, String nn) {
+	public void register(int t_y, int t_m, int t_d, String ln, String rn, String nn, String t_id, String t_pw) {
 		BirthY = t_y;
 		BirthM = t_m;
 		BirthD = t_d;
@@ -62,9 +93,30 @@ class User implements Serializable  {
 		LastName = ln;
 		RealName = rn;
 		NickName = nn;
+		
+		id = t_id;
+		pw = t_pw;
 	}
 	
-	public void Print() {
-		System.out.println(LastName + " " + RealName);
+	public void print() {
+		System.out.println(id + " " + pw);
+	}
+	
+	public boolean idCompare(String t_id) {
+		if(id.equals(t_id)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean pwCompare(String t_pw) {
+		if(pw.equals(t_pw)) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
